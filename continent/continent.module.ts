3b23909ee6ef303b8712d630796bd5b10d683761
Dydja <kadidjabayoko@gmail.com>
2022-10-19 23:54:49 +0100

    Cors errors

--- continent/continent.module.ts
+++ continent/continent.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContinentRoutingModule } from './continent-routing.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     ContinentRoutingModule
